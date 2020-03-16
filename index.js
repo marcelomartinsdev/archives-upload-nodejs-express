d31a76568f4105a2cf2167d641e1a7bdc80705fe
Marcelo Martins <marcelomartinsdev@gmail.com>
2020-03-16 19:04:26 -0300

    basic multer config done
    configured multer as a middleware catching uploaded archives and put on upload folder

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+const multer = require('multer');
 
 
 app.set('view engine','ejs');
 
-
+const upload = multer({dest: "upload/"}) // multer basic configuration (multer it's a middleware!)
 
 
 app.get("/",(req,res) => { 
   res.render("index");
 })
 
+app.post("/upload",upload.single("file"),(req,res) => { //make archive upload with multer to uploads folder through "file" button
+    res.send("Archive received!!");
+})
+
 app.listen(8080,() =>{
-    console.log("Servidor rodando!!");
+    console.log("Server is RUNNING!!");
 });
